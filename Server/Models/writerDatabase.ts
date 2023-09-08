6dc6fe7de5daa624d45dbfd43ae0b4d064ebcb42
Jin <jin.chong@mrjinit.com>
2023-09-09 01:48:53 +0800

    Create writedatabase model and tests

---
+++ Server/Models/writerDatabase.ts
@@ -0,0 +1,16 @@
+export class WriterDatabase { //SELF NOTE: Is this class really needed only two functions? Yes, because what if client wants to get the original writer
+    private writersList: Array<string> = []; //SELF NOTE: Why not assign in constructor? Adheres to Field initializers coding standard 
+
+    AddWriter(newWriter: string) { //SELF NOTE: Why not add this implemention into article class? Because violates S in Solid
+        if(newWriter !== "") {
+            this.writersList.push(newWriter);
+        } else {
+           throw new Error("error: writer needs to have a name");
+        }
+    }
+
+    get WritersList(): Array<string> { //SELF NOTE: Doesn't this violate encapsulation? Yes, but class/method is not for client to use but for article class which encapsulates this return
+        return this.writersList;
+    }
+
+}
---
+++ Tests/writerDatabase.tests.ts
@@ -0,0 +1,28 @@
+import { WriterDatabase } from '../Server/Models/writerDatabase'
+
+
+
+describe('---Test Cases: writerDatabase.ts----', () => {
+    test('add a writer to writerDatabase', () => {
+      let testVariable = new WriterDatabase();
+      testVariable.AddWriter("Tom");
+      testVariable.AddWriter("luke");
+      expect(testVariable.WritersList).toStrictEqual(["Tom", "luke"]);
+    });
+
+    test('add a empty string to writerDatabase', () => {
+        let testVariable = new WriterDatabase();
+        expect(() => { 
+            testVariable.AddWriter(""); 
+          }).toThrow();
+    });
+
+    test('get writers from writerDatabase', () => {
+        let testVariable = new WriterDatabase();
+        testVariable.AddWriter("Tom");
+        testVariable.AddWriter("luke");
+        expect(testVariable.WritersList).toStrictEqual(["Tom", "luke"]);
+      });
+  
+   
+  });

098d457ef018a817873379460e027cce86a233c8
Jin <jin.chong@mrjinit.com>
2023-09-09 14:27:07 +0800

    fix conflict

--- Tests/writerDatabase.tests.ts
+++ Tests/writerDatabase.tests.ts
@@ -6,8 +6,8 @@ describe('---Test Cases: writerDatabase.ts----', () => {
     test('add a writer to writerDatabase', () => {
       let testVariable = new WriterDatabase();
       testVariable.AddWriter("Tom");
-      testVariable.AddWriter("luke");
-      expect(testVariable.WritersList).toStrictEqual(["Tom", "luke"]);
+      testVariable.AddWriter("luKe");
+      expect(testVariable.WritersList).toStrictEqual(["Tom", "luKe"]);
     });
 
     test('add a empty string to writerDatabase', () => {

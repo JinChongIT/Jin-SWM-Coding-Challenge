c15b1057df180fd1e2034e38eae489584d9685fa
Jin <jin.chong@mrjinit.com>
2023-09-09 15:33:14 +0800

    Create utility functions for date and time

---
+++ Tests/dateTimeUtils.tests.ts
@@ -0,0 +1,22 @@
+import { ConvertToWADate, ConvertToWATime } from '../Server/Typescript/Utils/dateTimeUtils'
+
+describe('---Test Cases: dateTimeUtils.ts----', () => {
+    test('convert to wa time when date is valid', () => {
+    const tmpDate = new Date("2020-09-01T01:00:00.000Z");
+    const tmpTime = ConvertToWATime(tmpDate);
+      expect(tmpTime).toBe("9:00:00 am");
+    });
+
+    test('convert to wa time when date is not set', () => {
+      const tmpDate = new Date();
+      const tmpTime = ConvertToWATime(tmpDate);
+      expect(tmpTime).toBe(tmpDate.toLocaleTimeString());
+    });
+
+    test('convert to wa date when date is valid', () => {
+      const tmpDate = new Date("2020-11-30T01:00:00.000Z");
+      const conTmpTime = ConvertToWADate(tmpDate);
+        expect(conTmpTime).toBe("Mon, 30 Nov 2020");
+      });
+
+})
